308ad9641ea030b062ca5ef06af5c651a73aeda4
JuanRRodrigues <juancontato27@gmail.com>
2022-09-05 22:44:31 -0300

    Create I3 .c

---
+++ Livro Augusto Manzano/Capítulo 3/I3 .c
@@ -0,0 +1,30 @@
+/********************************************************************************
+*   File:    I3.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+
+/* Efetuar a leitura de um valor numérico inteiro e apresentar o resultado do valor lido elevado ao quadrado.  */
+
+
+
+int main()
+{
+    
+    int A, R;
+    printf("Digite um número inteiro para ser elevado ao quadrado:");
+    scanf("%i", &A);
+    R=pow(A,2);
+    
+    printf("O resultado é:%i", R);
+    
+    return 0;
+    
+    
+    
+    
+    
+}
