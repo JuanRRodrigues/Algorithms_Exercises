594b32b0d517e4cae22781a5f20f79a29d5df97c
JuanRRodrigues <juancontato27@gmail.com>
2022-09-09 23:05:21 -0300

    Create V3.c

---
+++ Livro Augusto Manzano/Capítulo 3/V3.c
@@ -0,0 +1,28 @@
+/********************************************************************************
+*   File:    U3.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+/* Elaborar um programa que leia dois valores numéricos inteiros, os quais devem representar a
+base e o expoente de uma potência, calcule a potência e apresente o resultado obtido.*/
+
+int main()
+{
+    
+    int A,B;
+    A = pow(A,B);
+    
+    printf("Digite A:");
+    scanf("%i", &A);
+    printf("Digite B:");
+    scanf("%i", &B);
+    A = pow(A,B);
+    printf("O volume eh:%i", A);
+    
+    
+    return 0;
+}
+    
