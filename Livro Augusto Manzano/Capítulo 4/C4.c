ed932148085e9c4b45e466869618fad6d615ba3a
JuanRRodrigues <juancontato27@gmail.com>
2022-09-10 21:53:05 -0300

    Create C4.c

---
+++ Livro Augusto Manzano/Capítulo 4/C4.c
@@ -0,0 +1,23 @@
+/********************************************************************************
+*   File:    C4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/* c) Realizar a leitura dos valores de quatro notas escolares bimestrais de um aluno representadas
+pelas variáveis N1, N2, N3 e N4. Calcular a média aritmética (variável MD) desse aluno
+e apresentar a mensagem "Aprovado" se a média obtida for maior ou igual a 5; caso contrário,
+apresentar a mensagem "Reprovado". Informar também, após a apresentação das mensagens, o
+valor da média obtida pelo aluno . */
+
+int main()
+{
+    
+    
+    
+    return 0;
+}
