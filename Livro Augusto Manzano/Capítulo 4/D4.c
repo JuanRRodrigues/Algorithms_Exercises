789c5c4f5c3184be607fa9fe556c468bb7b98e20
JuanRRodrigues <juancontato27@gmail.com>
2022-09-10 21:53:07 -0300

    Create D4.c

---
+++ Livro Augusto Manzano/Capítulo 4/D4.c
@@ -0,0 +1,26 @@
+/********************************************************************************
+*   File:    D4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/*d) Ler os valores de quatro notas escolares bimestrais de um aluno representadas pelas variáveis
+N1, N2, N3 e N4. Calcular a média aritmética (variável MD1) desse aluno e apresentar a
+mensagem "Aprovado" se a média obtida for maior ou igual a 7; caso contrário, o programa deve
+solicitar a quinta nota (nota de exame, representada pela variável NE) do aluno e calcular uma
+nova média aritmética (variável MD2) entre a nota de exame e a primeira média aritmética. Se o
+valor da nova média for maior ou igual a cinco, apresentar a mensagem "Aprovado em exame";
+caso contrário, apresentar a mensagem "Reprovado". Informar também, após a apresentação das
+mensagens, o valor da média obtida pelo aluno.  */
+
+int main()
+ {
+   
+     
+     
+     return 0;
+ }
