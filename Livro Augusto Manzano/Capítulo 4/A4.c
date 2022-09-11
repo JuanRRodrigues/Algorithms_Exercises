a146cbd4f819091ba50f4a225069da3a44d2c225
JuanRRodrigues <juancontato27@gmail.com>
2022-09-10 21:53:01 -0300

    Create A4.c

---
+++ Livro Augusto Manzano/Capítulo 4/A4.c
@@ -0,0 +1,20 @@
+/********************************************************************************
+*   File:    A4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+/* a) Efetuar a leitura de dois valores numéricos inteiros representados pelas variáveis A e B e
+apresentar o resultado da diferença do maior valor pelo menor valor. */
+
+#include <stdio.h>
+#include <locale.h>
+int main() 
+{
+  
+      
+       
+
+    return 0;
+}
+
