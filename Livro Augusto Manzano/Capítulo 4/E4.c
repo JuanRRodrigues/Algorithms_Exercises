ab82a0542e11acdb00d2869165362abf912a0dd0
JuanRRodrigues <juancontato27@gmail.com>
2022-09-10 21:53:09 -0300

    Create E4.c

---
+++ Livro Augusto Manzano/Capítulo 4/E4.c
@@ -0,0 +1,25 @@
+/********************************************************************************
+*   File:    E4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/* e) Efetuar a leitura de três valores numéricos (representados pelas variáveis A, B e C) e processar o
+cálculo da equação completa de segundo grau, utilizando a fórmula de Bhaskara (considerar para
+a solução do problema todas as possíveis condições para delta: delta < O - não há solução real,
+delta> O - há duas soluções reais e diferentes e delta= O - há apenas uma solução real). Lembre-
+-se de que é completa a equação de segundo grau que possui todos os coeficientes A, B e C
+diferentes de zero. O programa deve apresentar respostas para todas as condições estabelecidas
+para delta.  */
+
+ int main()
+ {
+    
+     
+     
+     return 0;
+ }
