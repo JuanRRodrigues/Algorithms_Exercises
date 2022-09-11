6e769c6f39d8b208cbf68570a3f76e39f893c13f
JuanRRodrigues <juancontato27@gmail.com>
2022-09-11 11:19:08 -0300

    Update B4.c

--- Livro Augusto Manzano/Capítulo 4/B4.c
+++ Livro Augusto Manzano/Capítulo 4/B4.c
@@ -1,20 +1,30 @@
 /********************************************************************************
 *   File:    B4.c                                                                *
 *   Author:  Juan R. Rodrigues                                                   *                                     
-*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*   Book: Algoritmos: Logica para desenvolvimento de programacao de computadores *
 *********************************************************************************/
 
 #include <stdio.h>
 #include <locale.h>
 
-/* b) Efetuar a leitura de um valor numérico inteiro positivo ou negativo representado pela variável N e
+/* b) Efetuar a leitura de um valor numerico inteiro positivo ou negativo representado pela variavel N e
 apresentar o valor lido como sendo positivo. Dica: se o valor lido for menor que zero, ele deve ser
 multiplicado por -1. 
  */
 
 int main()  
 {
-   
-    
+  // Ler
+  int N;
+  printf("digite um numero positivo ou negativo: ");
+  scanf("%i", &N);
+
+  // Decisão
+  if(N < 0){
+    N = N*(-1);
+  }
+  // Escrever
+  printf("o Valor digitado eh: %i", N);
+
     return 0;
 }
