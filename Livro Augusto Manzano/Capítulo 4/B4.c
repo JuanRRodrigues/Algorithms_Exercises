1eb47cbe6aadd65c44a5b838996b537597309237
JuanRRodrigues <juancontato27@gmail.com>
2022-09-10 21:53:03 -0300

    Create B4.c

---
+++ Livro Augusto Manzano/Capítulo 4/B4.c
@@ -0,0 +1,20 @@
+/********************************************************************************
+*   File:    B4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <locale.h>
+
+/* b) Efetuar a leitura de um valor numérico inteiro positivo ou negativo representado pela variável N e
+apresentar o valor lido como sendo positivo. Dica: se o valor lido for menor que zero, ele deve ser
+multiplicado por -1. 
+ */
+
+int main()  
+{
+   
+    
+    return 0;
+}
