09ec8500c236a2079359edb6f3865d9dd15ba596
JuanRRodrigues <juancontato27@gmail.com>
2022-09-12 22:48:41 -0300

    Create G4.c

---
+++ Livro Augusto Manzano/Capítulo 4/G4.c
@@ -0,0 +1,20 @@
+/********************************************************************************
+*   File:    G4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/* Fazer a leitura de quatro valores numéricos inteiros representados pelas variáveis A, B, C e D.
+Apresentar apenas os valores que sejam divisíveis por 2 e 3. 
+*/
+
+ int main()
+ {
+ 
+  
+     return 0;
+ }
