77a21704b7df4bb0947808e9a7f880000c73a6e4
JuanRRodrigues <juancontato27@gmail.com>
2022-09-13 20:56:02 -0300

    Create K4.c

---
+++ Livro Augusto Manzano/Capítulo 4/K4.c
@@ -0,0 +1,18 @@
+/********************************************************************************
+*   File:    K4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/*  
+*/
+
+ int main()
+ {
+ 
+     return 0;
+ }
