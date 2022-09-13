e8341941a19d69aaf8709f4b50796deee8f11a64
JuanRRodrigues <juancontato27@gmail.com>
2022-09-12 22:48:44 -0300

    Create H4 .c

---
+++ Livro Augusto Manzano/Capítulo 4/H4 .c
@@ -0,0 +1,19 @@
+/********************************************************************************
+*   File:    H4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/* Efetuar a leitura de quatro valores numéricos inteiros representados pelas variáveis A, B, C e D.
+Apresentar apenas os valores que sejam divisíveis por 2 ou 3. */
+
+ int main()
+ {
+
+  
+     return 0;
+ }
