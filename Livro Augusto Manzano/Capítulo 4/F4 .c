ea5c6083bb5da2039ea0fc14116b0377d8b3310b
JuanRRodrigues <juancontato27@gmail.com>
2022-09-12 22:48:39 -0300

    Create F4 .c

---
+++ Livro Augusto Manzano/Capítulo 4/F4 .c
@@ -0,0 +1,23 @@
+/********************************************************************************
+*   File:    F4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/* Ler três valores inteiros representados pelas variáveis A, B e C e apresentar os valores lidos
+dispostos em ordem crescente. Dica: utilizar tomada de decisão sequencial e as ideias trabalhadas
+nos exercícios "g" (propriedade distributiva) e "f' (troca de valores) do capítulo 3.  */
+
+ int main()
+ {
+ 
+        
+    
+
+  
+     return 0;
+ }
