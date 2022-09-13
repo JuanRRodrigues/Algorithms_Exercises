3440ff7f74054431de1d09e1dd2031ccbe8c0358
JuanRRodrigues <juancontato27@gmail.com>
2022-09-13 20:54:21 -0300

    Create J4.c

---
+++ Livro Augusto Manzano/Capítulo 4/J4.c
@@ -0,0 +1,19 @@
+/********************************************************************************
+*   File:    J4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/* Ler um valor numérico inteiro e apresentar uma mensagem informando se o valor fornecido é par
+ou ímpar. . 
+*/
+
+ int main()
+ {
+ 
+     return 0;
+ }
