44ca982b1ea3f3bd6879905b6874f0fc9ad5ae03
JuanRRodrigues <juancontato27@gmail.com>
2022-09-14 21:12:51 -0300

    Create N.c

---
+++ Livro Augusto Manzano/Capítulo 4/N.c
@@ -0,0 +1,20 @@
+/********************************************************************************
+*   File:    N4.c                                                                *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+*********************************************************************************/
+
+#include <stdio.h>
+#include <math.h>
+#include <locale.h>
+
+/*  
+Efetuar a leitura de três valores inteiros desconhecidos representados pelas variáveis A, B e C.
+Somar os valores fornecidos e apresentar o resultado somente se for maior ou igual a 100. ".  
+*/
+
+ int main()
+ {
+     
+     return 0;
+ }
