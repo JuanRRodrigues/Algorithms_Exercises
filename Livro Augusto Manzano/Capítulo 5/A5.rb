a75c70292aa53d6cd65ba8df894eed8f11edd767
JuanRRodrigues <juancontato27@gmail.com>
2022-09-16 22:22:20 -0300

    Create A5.rb

---
+++ Livro Augusto Manzano/Capítulo 5/A5.rb
@@ -0,0 +1,15 @@
+=begin
+**********************************************************************************
+*   File:    A5.rb                                                               *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+**********************************************************************************
+
+Elaborar um programa que apresente como resultado os quadrados dos números inteiros
+existentes na faixa de valores de 15 a 200. 
+
+
+=end
+
+puts "Quadrados de numeros inteiros existentes na faixa de 15 a 200"
+
