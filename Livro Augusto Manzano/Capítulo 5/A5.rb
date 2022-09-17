d4f376fb782b114a71e2b37f72ad2bb3b6a5c13b
JuanRRodrigues <juancontato27@gmail.com>
2022-09-17 14:20:41 -0300

    Update A5.rb

--- Livro Augusto Manzano/Capítulo 5/A5.rb
+++ Livro Augusto Manzano/Capítulo 5/A5.rb
@@ -10,6 +10,19 @@ existentes na faixa de valores de 15 a 200.
 
 
 =end
-
+count = 1
+resposta = 0
 puts "Quadrados de numeros inteiros existentes na faixa de 15 a 200"
+
+loop do
+    resposta = (count ** 2)
+    if resposta >= 15 && resposta <= 200
+    puts resposta
+end
+    if resposta >= 200
+        break
+    end
+    count += 1
+    
+end
 
