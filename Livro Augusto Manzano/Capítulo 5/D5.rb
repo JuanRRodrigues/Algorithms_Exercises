db0a071c7526a3b22a82e717f39e35d09f669767
JuanRRodrigues <juancontato27@gmail.com>
2022-09-18 21:51:19 -0300

    Delete D5.rb

--- Livro Augusto Manzano/Capítulo 5/D5.rb
+++
@@ -1,9 +0,0 @@
-=begin
-**********************************************************************************
-*   File:    D5.rb                                                               *
-*   Author:  Juan R. Rodrigues                                                   *                                     
-*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
-**********************************************************************************
-Elaborar um programa que apresente o somatório dos valores pares existentes na faixa de 1 até 500. 
- 
-=end
