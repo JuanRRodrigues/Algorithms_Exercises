b134665c245b2fec439490977ad2ac5c33afc547
JuanRRodrigues <juancontato27@gmail.com>
2022-09-19 21:45:13 -0300

    Update D5.rb

--- Livro Augusto Manzano/Capítulo 5/Ruby/D5.rb
+++ Livro Augusto Manzano/Capítulo 5/Ruby/D5.rb
@@ -7,3 +7,17 @@
 Elaborar um programa que apresente o somatório dos valores pares existentes na faixa de 1 até 500. 
  
 =end
+
+puts "Soma dos valores pares existentes na faixa de 1 até 500"
+valor = 0
+resultado = 0
+
+while valor <= 500
+    valor += 1
+    resto = valor%2
+    if resto == 0
+        resultado = valor + resultado
+    end
+end
+
+puts "Soma:#{resultado}"
