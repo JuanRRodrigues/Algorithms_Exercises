2d69c3ad21e1fcfcfc5abcb6a8091bc26548a20f
JuanRRodrigues <juancontato27@gmail.com>
2022-09-19 21:45:11 -0300

    Create C5.c

---
+++ Livro Augusto Manzano/Capítulo 5/C/C5.c
@@ -0,0 +1,26 @@
+/*********************************************************************************
+*   File:    C5.rb                                                               *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+**********************************************************************************
+Construir um programa que apresente a soma dos cem primeiros números naturais
+(1+2+3+ ... +98+99+100). */
+#include <stdio.h>
+
+int main(){
+    int count, N, resposta; 
+printf("Soma dos cem primeros numeros naturais");
+
+
+for(count=0; count<=100;count++){
+    resposta = count + resposta;
+    
+
+    
+}
+  printf("\nSoma=%d", resposta);
+
+    
+}
+
+
