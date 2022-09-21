3c943b298745a9815162776c90189a6b767900a7
JuanRRodrigues <juancontato27@gmail.com>
2022-09-20 21:12:00 -0300

    Create G5.rb

---
+++ Livro Augusto Manzano/Capítulo 5/Ruby/G5.rb
@@ -0,0 +1,14 @@
+=begin
+**********************************************************************************
+*   File:    G5.rb                                                               *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+**********************************************************************************
+Elaborar um programa que apresente os resultados das potências do valor de base 3, elevado a um
+expoente que varie do valor O até o valor 15. O programa deve apresentar os valores 1, 3, 9. 27, ... ,
+14.348.907. Sugestão: leve em consideração as definições matemáticas do cálculo de potência, em
+que qualquer valor numérico diferente de zero elevado a zero é 1, e todo valor numérico elevado a 1
+é ele próprio. Não use em hipótese nenhuma o operador aritmético de exponenciação apresentado
+no capítulo 3; resolva o problema com a técnica de laço
+=end
+
