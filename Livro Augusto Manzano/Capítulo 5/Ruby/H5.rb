d1ba7b47965d8ae5729932d818d21559b4c27256
JuanRRodrigues <juancontato27@gmail.com>
2022-09-20 21:12:02 -0300

    Create H5.rb

---
+++ Livro Augusto Manzano/Capítulo 5/Ruby/H5.rb
@@ -0,0 +1,14 @@
+=begin
+**********************************************************************************
+*   File:    H5.rb                                                               *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+**********************************************************************************
+Escrever um programa que apresente como resultado a potência de uma base qualquer elevada
+a um expoente qualquer, ou seja, de BE, em que B é o valor da base e E o valor do expoente.
+Considere apenas a entrada de valores inteiros e positivos, ou seja, de valores naturais.
+Sugestão: não utilize o formato "base j expoente", pois é uma solução muito trivial. Use a
+técnica de laço, em que o valor da base deve ser multiplicado o número de vezes determinado
+no expoente. 
+=end
+
