bae2e306bd543efbdd61796c373346ebd2d621ee
JuanRRodrigues <juancontato27@gmail.com>
2022-09-20 21:11:58 -0300

    Update F5.rb

---
+++ Livro Augusto Manzano/Capítulo 5/Ruby/F5.rb
@@ -0,0 +1,10 @@
+=begin
+**********************************************************************************
+*   File:    F5.rb                                                               *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+**********************************************************************************
+Construir um programa que apresente todos os valores numéricos divisíveis por 4 e menores que
+200. Sugestão: a variável que controla o contador do laço deve ser iniciada com valor 1. 
+=end
+
