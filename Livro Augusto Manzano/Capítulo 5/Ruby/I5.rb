9507b042b59e0a4f486781175a7121715b662508
JuanRRodrigues <juancontato27@gmail.com>
2022-09-20 21:12:04 -0300

    Create I5.rb

---
+++ Livro Augusto Manzano/Capítulo 5/Ruby/I5.rb
@@ -0,0 +1,15 @@
+=begin
+**********************************************************************************
+*   File:    I5.rb                                                               *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+**********************************************************************************
+Escrever um programa que apresente os valores da sequência numérica de Fibonacci até o
+décimo quinto termo. A sequência de Fibonacci é formada por O, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+89, 144, 233, 377, ... etc., obtendo-se o próximo termo a partir da soma do termo atual com o
+anterior sucessivamente até o infinito, se a sequência não for interrompida. Utilize para este
+exercício as variáveis ATUAL, ANTERIOR e PRÓXIMO
+=end
+
+
+
