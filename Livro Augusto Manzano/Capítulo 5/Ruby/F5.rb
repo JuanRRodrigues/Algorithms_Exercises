18ce807482e6b5dce5050052208492b71abfd57e
JuanRRodrigues <juancontato27@gmail.com>
2022-09-22 23:54:48 -0300

    Update F5.rb

--- Livro Augusto Manzano/Capítulo 5/Ruby/F5.rb
+++ Livro Augusto Manzano/Capítulo 5/Ruby/F5.rb
@@ -8,3 +8,15 @@ Construir um programa que apresente todos os valores numéricos divisíveis por 
 200. Sugestão: a variável que controla o contador do laço deve ser iniciada com valor 1. 
 =end
 
+
+puts "Todos os números divisíveis por 4 e menores que 200"
+
+count = 0
+while count < 199
+    count += 1
+    resto = count%4
+    if resto == 0
+        puts count
+    end
+end
+
