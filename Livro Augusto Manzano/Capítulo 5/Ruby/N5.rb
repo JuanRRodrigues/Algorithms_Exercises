6ec00ca581fdcd5e723cde8aea85359075bc8093
JuanRRodrigues <juancontato27@gmail.com>
2022-10-03 23:24:50 -0300

    Update N5.rb

--- Livro Augusto Manzano/Capítulo 5/Ruby/N5.rb
+++ Livro Augusto Manzano/Capítulo 5/Ruby/N5.rb
@@ -4,9 +4,7 @@
 *   Author:  Juan R. Rodrigues                                                   *                                     
 *   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
 **********************************************************************************
-Elaborar um programa que apresente os valores de conversão de graus Celsius em graus
-Fahrenheit, de dez em dez graus, iniciando a contagem em dez graus Celsius e finalizando em
-cem graus Celsius. O programa deve apresentar os valores das duas temperaturas. 
+ 
 =end
 
 
