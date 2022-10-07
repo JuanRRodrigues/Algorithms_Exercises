0d7c8f8d74cde6f4d0abf6ac228b477dc5b06a67
JuanRRodrigues <juancontato27@gmail.com>
2022-10-06 23:42:42 -0300

    Update L5.rb

--- Livro Augusto Manzano/Capítulo 5/Ruby/L5.rb
+++ Livro Augusto Manzano/Capítulo 5/Ruby/L5.rb
@@ -1,10 +1,11 @@
 =begin
 **********************************************************************************
-*   File:    J5.rb                                                               *
+*   File:    L5.rb                                                               *
 *   Author:  Juan R. Rodrigues                                                   *                                     
 *   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
 **********************************************************************************
-
+Elaborar um programa que leia quinze valores numéricos inteiros e no final apresente o
+somatório da fatorial de cada valor lido.
 =end
 
 
