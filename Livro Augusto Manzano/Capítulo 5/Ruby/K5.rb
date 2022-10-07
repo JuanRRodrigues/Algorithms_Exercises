faec0fce98dc46d21982440b58eeff21e68ef6c0
JuanRRodrigues <juancontato27@gmail.com>
2022-10-06 23:42:40 -0300

    Update K5.rb

--- Livro Augusto Manzano/Capítulo 5/Ruby/K5.rb
+++ Livro Augusto Manzano/Capítulo 5/Ruby/K5.rb
@@ -4,8 +4,16 @@
 *   Author:  Juan R. Rodrigues                                                   *                                     
 *   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
 **********************************************************************************
+Escrever um programa que calcule e apresente o somatório do número de grãos de trigo que se
+pode obter num tabuleiro de xadrez, obedecendo à seguinte regra: colocar um grão de trigo no
+primeiro quadro e nos quadros seguintes o dobro do quadro anterior. Ou seja, no primeiro
+quadro coloca-se um grão, no segundo quadro colocam-se dois grãos (neste momento, tem-se
+três grãos), no terceiro quadro colocam-se quatro grãos (tendo neste momento sete grãos),
+no quarto quadro colocam-se oito grãos (tendo-se então 15 grãos) até atingir o sexagésimo
+quarto quadro (este exercício foi baseado numa situação exposta no capítulo 16 do livro
+"O Homem que Calculava" de Malba Tahan, da Editora Record). 
 
 =end
 
-
+I=0
 
