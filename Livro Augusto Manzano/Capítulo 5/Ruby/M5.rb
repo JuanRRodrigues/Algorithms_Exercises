e5e8482be20e9bb9014902692d018ef056bc491c
JuanRRodrigues <juancontato27@gmail.com>
2022-10-07 22:39:28 -0300

    Create M5.rb

---
+++ Livro Augusto Manzano/Capítulo 5/Ruby/M5.rb
@@ -0,0 +1,11 @@
+=begin
+**********************************************************************************
+*   File:    J5.rb                                                               *
+*   Author:  Juan R. Rodrigues                                                   *                                     
+*   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
+**********************************************************************************
+ 
+=end
+
+
+
