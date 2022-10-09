9eba2f351ac2e1e7a0c18b674ffad1b1ffe48cce
JuanRRodrigues <juancontato27@gmail.com>
2022-10-08 23:50:27 -0300

    Update L5.rb

--- Livro Augusto Manzano/Capítulo 5/Ruby/L5.rb
+++ Livro Augusto Manzano/Capítulo 5/Ruby/L5.rb
@@ -8,5 +8,20 @@ Elaborar um programa que leia quinze valores numéricos inteiros e no final apre
 somatório da fatorial de cada valor lido.
 =end
 
+numeros = []
+i = 0
 
-
+while i <= 14
+    puts "digite um valor inteiro:"
+    numeros[i] = gets.to_i
+    f = numeros[i]-1
+    valoratual = numeros[i]
+    while f > 0
+        
+        numeros[i] = valoratual*f
+        valoratual = numeros[i]
+        f -= 1
+end
+puts "fatorial #{i}: #{numeros[i]}"
+i += 1
+end
