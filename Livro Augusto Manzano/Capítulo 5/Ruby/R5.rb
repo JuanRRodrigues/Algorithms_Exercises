3c631c0ccf925a198ee60678c5cfa08055409ee6
JuanRRodrigues <juancontato27@gmail.com>
2022-10-10 23:46:06 -0300

    Update R5.rb

--- Livro Augusto Manzano/Capítulo 5/Ruby/R5.rb
+++ Livro Augusto Manzano/Capítulo 5/Ruby/R5.rb
@@ -4,7 +4,8 @@
 *   Author:  Juan R. Rodrigues                                                   *                                     
 *   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
 **********************************************************************************
- 
+ Elaborar um programa que leia valores positivos inteiros até que um valor negativo seja
+informado. Ao final devem ser apresentados o maior e o menor valores informados pelo usuário.
 =end
 
 
