b4581e861c8516d1d0c887c59eebdeea0f5ee3ec
JuanRRodrigues <juancontato27@gmail.com>
2022-10-11 23:02:53 -0300

    Update N5.rb

--- Livro Augusto Manzano/Capítulo 5/Ruby/N5.rb
+++ Livro Augusto Manzano/Capítulo 5/Ruby/N5.rb
@@ -1,10 +1,13 @@
 =begin
 **********************************************************************************
-*   File:    J5.rb                                                               *
+*   File:    N5.rb                                                               *
 *   Author:  Juan R. Rodrigues                                                   *                                     
 *   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
 **********************************************************************************
- 
+ Elaborar um programa que leia sucessivamente valores numéricos e apresente no final
+o somatório, a média e o total de valores lidos. O programa deve ler os valores enquanto o
+usuário estiver fornecendo valores positivos. Ou seja, o programa deve parar quando o usuário
+fornecer um valor negativo (menor que zero).
 =end
 
 
