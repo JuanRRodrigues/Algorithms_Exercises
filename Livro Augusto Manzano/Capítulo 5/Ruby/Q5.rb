998512ac0f4e5373becacd1fff8760a29cad9fb3
JuanRRodrigues <juancontato27@gmail.com>
2022-10-19 23:17:07 -0300

    Update Q5.rb

--- Livro Augusto Manzano/Capítulo 5/Ruby/Q5.rb
+++ Livro Augusto Manzano/Capítulo 5/Ruby/Q5.rb
@@ -1,10 +1,16 @@
 =begin
 **********************************************************************************
-*   File:    QQ5.rb                                                               *
+*   File:    Q5.rb                                                               *
 *   Author:  Juan R. Rodrigues                                                   *                                     
 *   Book: Algoritmos: Lógica para desenvolvimento de programação de computadores *
 **********************************************************************************
- 
+ Escrever um programa que possibilite calcular a área total em metros de uma residência com os
+cômodos sala, cozinha, banheiro, dois quartos, área de serviço, quintal, garagem, entre outros,
+que podem ser fornecidos ao programa. O programa deve solicitar a entrada do nome, da
+largura e do comprimento de um determinado cômodo. Em seguida, deve apresentar a área do
+cômodo lido e também uma mensagem solicitando ao usuário a confirmação de continuar
+calculando novos cômodos. Caso o usuário responda "NÃO", o programa deve apresentar o
+valor total acumulado da área residencial. 
 =end
 
 
