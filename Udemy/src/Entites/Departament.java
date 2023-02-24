53b3eb3a1c601a773e6b2c59428a23ec2cb61316
JuanRRodrigues <juancontato27@gmail.com>
2023-02-23 22:26:08 -0300

    Create Departament.java

---
+++ Udemy/src/Entites/Departament.java
@@ -0,0 +1,23 @@
+package Entites;
+
+public class Departament {
+  private String name;
+  
+  public Departament(){
+	  
+  }
+
+public Departament(String name) {
+	
+	this.name = name;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+}
